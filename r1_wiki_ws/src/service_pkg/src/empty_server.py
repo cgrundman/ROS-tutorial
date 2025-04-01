7b4e9e34e79a04be50d253ac02473e221883b148
cgrundman <christian.grundman@gmail.com>
2025-04-01 15:12:23 +0200

    add basic function to empty service

--- r1_wiki_ws/src/service_pkg/src/empty_server.py
+++ r1_wiki_ws/src/service_pkg/src/empty_server.py
@@ -2,12 +2,23 @@
 
 from std_srvs.srv import Empty, EmptyResponse
 import rospy
+from geometry_msgs.msg import Twist
 
-def empty_responce(req):
+def empty_responce_cb(req):
+    vel_msg = Twist()
+    for i in range(10):
+        vel_msg.linear.x = 1
+        vel_msg.angular.z = 0.5
+        pub.publish(vel_msg)
+        rate.sleep()
     print("Just a service")
     return EmptyResponse()
 
 if __name__ == "__main__":
+    rospy.logwarn("Starting Empty Server")
     rospy.init_node('empty_server_service')
-    s = rospy.Service('print_service', Empty, empty_responce)
+    pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size=2)
+    s = rospy.Service('print_service', Empty, empty_responce_cb)
+    rate = rospy.Rate(1)
+
     rospy.spin()
